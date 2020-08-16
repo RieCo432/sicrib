6e5e7e0bf160876bc15db1eb1731c049f2741a2a
Colin Ries <colin.ries@gmail.com>
2020-08-16 03:13:10 +0100

    played with effect

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ living_room = build_living_room()
 time.sleep(2)
 
 led_list_in_order = living_room.build_list_horizontal_circle()
+#led_list_in_order = living_room.build_list_vertical_straight()
+
 timestamp_start = datetime.now()
 while True:
     time_elapsed = (datetime.now() - timestamp_start).total_seconds()
-    living_room.set_hue_span_color_cycle(led_list_in_order, time_elapsed=time_elapsed)
+    living_room.set_hue_span_color_cycle(led_list_in_order, compress=4, speed=23, time_elapsed=time_elapsed)
+    living_room.set_led_rgb(464, (0,0,0))
+    #living_room.set_led_rgb(121, (255, 255, 255))
     living_room.update()
