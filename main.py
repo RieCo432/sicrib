bd02726a838ed07d7129137ba824ed594e80ee1c
Colin Ries <colin.ries@gmail.com>
2020-08-16 05:20:14 +0100

    set brightness

--- main.py
+++ main.py
@@ -14,20 +14,21 @@ timestamp_start = datetime.now()
 last_ceiling_stamp = last_vertical_stamp = datetime.now()
 iter_duration_stamp = datetime.now()
 iter_counter = 0
+living_room.brightness = 1.0
 while True:
 
     time_elapsed = (datetime.now() - timestamp_start).total_seconds()
 
-    calc_time_start = datetime.now()
-    #living_room.set_hue_span_color_cycle(led_list_in_order, compress=4, speed=23, time_elapsed=time_elapsed)
-    last_ceiling_stamp, last_vertical_stamp = living_room.christmas_animation(last_ceiling_stamp, last_vertical_stamp)
-    print((datetime.now() - calc_time_start).total_seconds(), "calc time")
+    #calc_time_start = datetime.now()
+    living_room.set_hue_span_color_cycle(led_list_in_order, compress=4, speed=23, time_elapsed=time_elapsed)
+    #last_ceiling_stamp, last_vertical_stamp = living_room.christmas_animation(last_ceiling_stamp, last_vertical_stamp)
+    #print((datetime.now() - calc_time_start).total_seconds(), "calc time")
 
     living_room.set_led_rgb(464, (0,0,0))
 
-    update_time_start = datetime.now()
+    #update_time_start = datetime.now()
     living_room.update()
-    print((datetime.now() - update_time_start).total_seconds(), "update time")
+    #print((datetime.now() - update_time_start).total_seconds(), "update time")
 
     # speed analysis
     if iter_counter == 99:
