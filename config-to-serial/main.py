02a58d4d34dc5fc5dbc477ffb011e3908618fb03
Colin Ries <colin.ries@gmail.com>
2021-02-01 02:15:01 +0000

    added retry routine for establishing serial connection

--- config-to-serial/main.py
+++ config-to-serial/main.py
@@ -5,8 +5,21 @@ import serial
 
 fx_config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "share", "fx_config.json")
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.flush()
+connected = False
+attempts_left = 60
+while not connected and attempts_left > 1:
+    try:
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
+        ser.flush()
+        connected = True
+        print("connected!")
+    except serial.serialutil.SerialException:
+        sleep(5)
+        attempts_left -= 1
+        pass
+
+if not connected:
+    exit(1)
 
 def load_fx_config():
     while True:
